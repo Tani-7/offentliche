704c056de33cf40057aea9a16f3338a0a15ab886
Tani-7 <ohmonesir@outlook.com>
2025-07-06 09:40:52 +0300

    Fehler

--- automatisierung.py
+++ automatisierung.py
@@ -1,13 +1,13 @@
 import argparse
 
-def main(number, other_number):
-    result = number * other_number
-    print(f'The result is {result}')
+def berechne_ergebnis(zahl1, zahl2):
+    ergebnis = zahl1 * zahl2
+    print(f'Das Ergebnis ist {ergebnis}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n1', type=int, help='A number', default=1)
-    parser.add_argument('-n2', type=int, help='Another number', default=1)
+    parser.add_argument('-z1', type=int, help='Eine Zahl', default=1)
+    parser.add_argument('-z2', type=int, help='Eine weitere Zahl', default=1)
 
     args = parser.parse_args()
-    main(args.n1, args.n2)
+    berechne_ergebnis(args.z1, args.z2)
--- config.py
+++ config.py
@@ -1,4 +1,24 @@
 import argparse
 import configparser
 
-def main(nummer, andere_nummer)
+def main(zahl1, zahl2):
+    ergebnis = zahl1 * zahl2
+    print(f'Das Ergebnis ist {ergebnis}')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-z1', type=int, help='Eine Zahl', default=1)
+    parser.add_argument('-z2', type=int, help='Eine weitere Zahl', default=1)
+    parser.add_argument('--config', '-c', type=argparse.FileType('r'), help='Konfigurationsdatei')
+
+    args = parser.parse_args()
+
+    if args.config:
+        config = configparser.ConfigParser()
+        config.read_file(args.config)
+
+        # Umwandlung der Werte in Integer
+        args.z1 = int(config['ARGUMENTE']['z1'])
+        args.z2 = int(config['ARGUMENTE']['z2'])
+
+    main(args.z1, args.z2)
