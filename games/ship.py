0ad699564e7c7e75d4f2c58f5c3035d23edd2177
Tani-7 <ohmonesir@outlook.com>
2025-07-08 14:23:29 +0300

    weg

--- games/ship.py
+++ games/ship.py
@@ -5,15 +5,35 @@ class Ship:
     def __init__(self, ai_game):
         # Start the ship.
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load and scale the ship image to ~1.5cm (≈ 57x57 pixels).
-        original_image = pygame.image.load('images/Marriete of 1915.bmp')
-        self.image = pygame.transform.smoothscale(original_image, (256, 256))
+        original_image = pygame.image.load('images/millenium_falcon.bmp')
+        self.image = pygame.transform.smoothscale(original_image, (120, 120))
         self.rect = self.image.get_rect()
 
         # Set the initial position of the ship.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #Adjusting the speed to use float values 
+        self.x  = float(self.rect.x)
+
+        #Allowing continuous movement on keypress THE FLAGS
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """updating the millenium's position based on the movement flag"""
+        #Modifying the ship's x-value, not affecting the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #updating the Rect from self.x
+        self.rect.x = self.x
+
 
     def blitme(self):
         # Draw the ship at its current location.
--- games/trial_game.py
+++ games/trial_game.py
@@ -3,47 +3,62 @@ import pygame
 from settings import Settings
 from ship import Ship
 
-class auslander:
-    # entry pouint to manage  the games assets and behaviour
+class Auslander:
+    """Main class to manage game assets and behavior."""
+    
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
-        self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption('Auslander Krieg')
 
-        self.bg_color = (230, 230, 230)
+        self.clock = pygame.time.Clock()
         self.ship = Ship(self)
 
-
     def run_game(self):
-        #THe games Main loop
+        """The game’s main loop."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
-        
+
     def _check_events(self):
-        """Responds to keypresses and mouse events."""
+        """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                    sys.exit()
-            #let's redraw the screen  with each pass of the loop
-    
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
     def _update_screen(self):
-        """Update the images on the screen and flip on the new screen"""
-        
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            #show the most recent drawing
-            pygame.display.flip()
-            
-    
+        """Update images on the screen and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
-    #initiate a game instance and run the game
-    ai = auslander()
+    # Create a game instance and run the game.
+    ai = Auslander()
     ai.run_game()
-
-               
